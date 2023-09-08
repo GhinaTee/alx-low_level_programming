4b6d8a2f1205551de4d64c4a46b18f315325f64a
GhinaTee <ignatioustee@gmail.com>
2023-09-07 17:56:58 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - the main function
  *
- * retun: 0 when a success
+ * Return: 0 when a success
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" -Dora Kor par, 2015-10-19\n", stdout);
+	fputs("and tht piece of art is useful\" -Dora Kor par, 2015-10-19\n", stdout);
 	return (0);
 }
